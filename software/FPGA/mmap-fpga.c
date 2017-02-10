45111222ee19a2c98521b8f21cc54168e5b8d2e2
pedro <hdlguy@hotmail.com>
2017-02-10 07:49:15 -0700

    added BRAM test to mmap-fpga.c

--- software/FPGA/mmap-fpga.c
+++ software/FPGA/mmap-fpga.c
@@ -28,6 +28,18 @@ int main(int argc,char** argv)
     fprintf(stdout,"FPGA ID: 0x%08X\n",read_reg(pcie_addr,FPGA_ID));
     fprintf(stdout,"VERSION: 0x%08X\n",read_reg(pcie_addr,FPGA_VERSION));
 
+
+    fprintf(stdout, "filling Test BRAM.\n");
+    uint32_t bram_data[TEST_BRAM_SIZE/4], bram_results[TEST_BRAM_SIZE/4];
+    for(uint32_t i=0; i<TEST_BRAM_SIZE/4; i++) bram_data[i] = i;
+    memcpy(pcie_addr+TEST_BRAM_OFFSET, bram_data, TEST_BRAM_SIZE);
+    fprintf(stdout, "reading Test BRAM.\n");
+    memcpy(bram_results, pcie_addr+TEST_BRAM_OFFSET, TEST_BRAM_SIZE);
+    int bram_errors = 0;
+    for(uint32_t i=0; i<TEST_BRAM_SIZE/4; i++) if(bram_data[i] != bram_results[i]) bram_errors++;
+    fprintf(stdout, "number of BRAM errors = %d\n", bram_errors);
+    
+
     munmap(pcie_addr,pcie_bar0_size);
 
     return 0;
